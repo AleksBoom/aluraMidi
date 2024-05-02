2fb36fb3ab416ef2e5c0538dbc60692ef8fcbd2d
aleksboom <noiseman85@gmail.com>
2024-05-01 23:48:19 -0600

    actualizacionJavascript

---
+++ main.js
@@ -0,0 +1,22 @@
+function playSonido (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');//Creamos un arreglo que contenga todas los elemntos
+//que contengan la clase .tecla
+
+let contador = 0
+
+while (contador < 9){
+   const tecla = listaDeTeclas[contador];
+   const instrumento = tecla.classList[1];//
+
+   const idAudio = `#sonido_${instrumento}`;
+   console.log(idAudio)
+
+   tecla.onclick = function (){
+    playSonido(idAudio);
+   }; 
+   contador = contador + 1;
+   
+}
